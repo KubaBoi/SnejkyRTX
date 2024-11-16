53f7924b057bf5e22f3568505ef791ead0f456c8
KubaBoi <jakubanderle@outlook.cz>
2024-11-16 20:56:13 +0100

    cx

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ triangles_count = 1
 
 camera_dir = (NUM_TYPE * CAMERA_SIZE)(100,0,0, 0,1,0, 0,0,1)
 lights = (NUM_TYPE * LIGHT_SIZE)(0,0,0, 0)
-triangles = (NUM_TYPE * TRIANGLE_SIZE)(2000,0,2000, # vertex 1
-                                        2000,-2000,-2000, # vertex 2
-                                        2000,2000,-2000, # vertex 3
+triangles = (NUM_TYPE * TRIANGLE_SIZE)(10,0,10, # vertex 1
+                                        10,-10,-10, # vertex 2
+                                        10,10,-10, # vertex 3
                                         25,25,0) # colors                     
 
-SCREEN_WIDTH = 200
-SCREEN_HEIGHT = 200
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
 HALF_HEIGHT = int(SCREEN_HEIGHT / 2)
 HALF_WIDTH = int(SCREEN_WIDTH / 2)
 
@@ -40,11 +40,11 @@ for y in range(SCREEN_HEIGHT):
                 lights, lights_count, 
                 triangles, triangles_count)
         if (color < 0): 
-            img.putpixel((x, y), (255, 255, 255))
+            img.putpixel((x, SCREEN_HEIGHT - y - 1), (255, 255, 255))
             continue
         #print(x, y, color)
         byte_arr = struct.pack("<I", color)
-        img.putpixel((x, y), (byte_arr[0], byte_arr[1], byte_arr[2]))
+        img.putpixel((x, SCREEN_HEIGHT - y - 1), (byte_arr[0], byte_arr[1], byte_arr[2]))
         #img.putpixel((x, y), (0,25,0))
 
 img.save("test.png", format="png")
